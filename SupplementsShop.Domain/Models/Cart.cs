f7d0e8444bac7e6288b05131e44ecf452e308c9a
diomedes-vba <vladyslav.babych2106@gmail.com>
2025-02-24 19:10:14 +0400

    Models without EF in Domain (Cart-related)

---
+++ SupplementsShop.Domain/Models/Cart.cs
@@ -0,0 +1,35 @@
+using SupplementsShop.Domain.Entities;
+
+namespace SupplementsShop.Domain.Models;
+
+public class Cart
+{
+    private List<CartItem> _items = new List<CartItem>();
+    public IReadOnlyList<CartItem> Items => _items.AsReadOnly();
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = _items.FirstOrDefault(p => p.Id == product.Id);
+
+        if (item == null)
+        {
+            _items.Add(new CartItem(
+                product.Id,
+                product.Name,
+                product.Price,
+                quantity,
+                product.ImageUrl
+                ));
+        }
+        else
+        {
+            item.IncreaseQuantity(quantity);
+        }
+    }
+    
+    public void RemoveItem(int productId) => _items.RemoveAll(p => p.Id == productId);
+    
+    public decimal TotalPrice => _items.Sum(p => p.TotalPrice);
+    
+    public void Clear() => _items.Clear();
+}
