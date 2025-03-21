a1ddd2941497b1fb8b21da935319e4a01e55c173
diomedes-vba <vladyslav.babych2106@gmail.com>
2025-03-21 23:40:45 +0400

    ToPagedList extension

---
+++ SupplementsShop.Infrastructure/Extensions/AsyncIQueryableExtensions.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using SupplementsShop.Domain.Interfaces;
+using SupplementsShop.Domain.Models;
+
+namespace SupplementsShop.Infrastructure.Extensions;
+
+public static class AsyncIQueryableExtensions
+{
+    /* getOnlyTotalCount - A value is indicating whether you want to only load the total number of records.
+    Set to true if you don't want to load data from database */
+    public static async Task<IPagedList<T>> ToPagedListAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize,
+        bool getOnlyTotalCount = false)
+    {
+        if (source == null)
+        {
+            return new PagedList<T>(new List<T>(), pageIndex, pageSize);
+        }
+
+        var count = await source.CountAsync();
+        
+        var data = new List<T>();
+
+        if (!getOnlyTotalCount)
+        {
+            data.AddRange(await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync());
+        }
+        
+        return new PagedList<T>(data, pageIndex, pageSize, count);
+    }
+}
