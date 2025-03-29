b473a8cbae0aa7559b9cc011da6913adb7756ec0
diomedes-vba <vladyslav.babych2106@gmail.com>
2025-03-30 00:04:32 +0100

    Previous and next page in category products

--- SupplementsShop/wwwroot/js/category.js
+++ SupplementsShop/wwwroot/js/category.js
@@ -1,14 +1,34 @@
 function bindPageButtons() {
-        document.querySelectorAll(".other-page-btn").forEach(btn => {
-            btn.addEventListener('click', function (event) {
-                event.preventDefault();
-                let categoryId = parseInt(this.getAttribute("data-category-id"));
-                let pageIndex = parseInt(this.getAttribute("data-page-number")) - 1;
-                let pageSize = parseInt(this.getAttribute("data-page-size"));
-                getProductsForPage(categoryId, pageIndex, pageSize);
-            });
+    document.querySelectorAll(".other-page-btn").forEach(btn => {
+        btn.addEventListener('click', function (event) {
+            event.preventDefault();
+            let categoryId = parseInt(this.getAttribute("data-category-id"));
+            let pageIndex = parseInt(this.getAttribute("data-page-number")) - 1;
+            let pageSize = parseInt(this.getAttribute("data-page-size"));
+            getProductsForPage(categoryId, pageIndex, pageSize);
         });
-    }
+    });
+    
+    document.querySelectorAll(".previous-page-btn").forEach(btn => {
+        btn.addEventListener('click', function (event) {
+            event.preventDefault();
+            let categoryId = parseInt(this.getAttribute("data-category-id"));
+            let pageIndex = parseInt(this.getAttribute("data-current-page")) - 1;
+            let pageSize = parseInt(this.getAttribute("data-page-size"));
+            getProductsForPage(categoryId, pageIndex, pageSize);
+        })
+    })
+
+    document.querySelectorAll(".next-page-btn").forEach(btn => {
+        btn.addEventListener('click', function (event) {
+            event.preventDefault();
+            let categoryId = parseInt(this.getAttribute("data-category-id"));
+            let pageIndex = parseInt(this.getAttribute("data-current-page")) + 1;
+            let pageSize = parseInt(this.getAttribute("data-page-size"));
+            getProductsForPage(categoryId, pageIndex, pageSize);
+        })
+    })
+}
     
 document.addEventListener('DOMContentLoaded', () => {
     bindPageButtons();
